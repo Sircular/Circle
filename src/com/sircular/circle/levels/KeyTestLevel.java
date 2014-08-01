7da217ed205812c3e6ea76c9975ec3612587f324
Sircular <Sircular@github.com>
2014-08-01 14:17:48 -0600

    Made it possible to actually exit the keyboard test.

--- src/com/sircular/circle/levels/KeyTestLevel.java
+++ src/com/sircular/circle/levels/KeyTestLevel.java
@@ -2,13 +2,18 @@ package com.sircular.circle.levels;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 
 import com.sircular.circle.engine.GameState;
 import com.sircular.circle.engine.Keyboard;
 import com.sircular.circle.engine.StateEngine;
 import com.sircular.circle.engine.TextRenderer;
+import com.sircular.circle.menus.MainMenu;
+import com.sircular.circle.menus.Menu1;
 
 public class KeyTestLevel extends GameState {
+	
+	private BufferedImage helpText = TextRenderer.renderText("Press ESC to Exit", 1, Color.GRAY);
 
 	public KeyTestLevel(StateEngine engine, int width, int height) {
 		super(engine, width, height);
@@ -32,7 +37,9 @@ public class KeyTestLevel extends GameState {
 		
 		g.setColor(Color.black);
 		g.fillRect(0, 0, this.width, this.height);
-		// bad to do this kind of thing in drawing, but waaay simpler this way.
+		
+		g.drawImage(helpText, width-(helpText.getWidth()+5), height-(helpText.getHeight()+5), null);
+		// bad to do this kind of thing in draw(), but waaay simpler this way.
 		int i = 0;
 		
 		Integer[] keySet = new Integer[Keyboard.getKeyMap().keySet().size()]; // THIS KIND OF THING IS RIDICULOUS, JAVA!
@@ -40,6 +47,14 @@ public class KeyTestLevel extends GameState {
 		
 		for (Integer key : keySet) {
 			if (Keyboard.isKeyDown(key)) {
+				// BAD, BAD WALT
+				if (key == 27) { // we've hit escape
+					MainMenu mainMenu = new MainMenu(engine, width, height);
+					Menu1 menu = new Menu1(engine, mainMenu, width, height);
+					mainMenu.setMenu(menu);
+					engine.setState(mainMenu);
+				}
+				
 				g.drawImage(TextRenderer.renderText(String.valueOf(key), 1, Color.WHITE), 2, (i*16)+2, null);
 				i++;
 			}
