5712825ef9695cf92f4d33f0079ce4d3c1e4bd2e
Sircular <Sircular@github.com>
2014-08-02 16:39:09 -0600

    Added factory method to create button using images instead of text.

--- src/com/sircular/circle/menus/ButtonFactory.java
+++ src/com/sircular/circle/menus/ButtonFactory.java
@@ -3,6 +3,7 @@ package com.sircular.circle.menus;
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 
+import com.sircular.circle.engine.ImageColorizer;
 import com.sircular.circle.engine.ImageLoader;
 import com.sircular.circle.engine.SlicedImage;
 import com.sircular.circle.engine.TextRenderer;
@@ -11,24 +12,33 @@ public class ButtonFactory {
 	
 	private final static String BUTTON_IMG_PATH = "/com/sircular/circle/data/assets/img/button.png";
 	
-	public static Button createTextButton(ButtonHandler parent, int id, int x, int y, int width, String text) {
+	public static Button createTextButton(ButtonHandler parent, int id, int x, int y, int width, String text) {		
+		BufferedImage textImg = TextRenderer.renderText(text, 2, Color.white);
+		
+		return createImageButton(parent, id, x, y, width, 48, textImg, true);
+	}
+	
+	public static Button createImageButton(ButtonHandler parent, int id, int x, int y, int width, int height, BufferedImage image, boolean shadow) {
 		BufferedImage[] sheet = ImageLoader.loadSpriteSheet(BUTTON_IMG_PATH, 48, 48);
 		BufferedImage[] imgs = new BufferedImage[sheet.length];
 		
 		for (int i = 0; i < sheet.length; i++) { 
 			BufferedImage img = sheet[i];
 			SlicedImage slice = new SlicedImage(img, 8, 24, 40, 25);
-			BufferedImage textImg = TextRenderer.renderText(text, 2, Color.white);
-			BufferedImage textImg2 = TextRenderer.renderText(text, 2, new Color(0, 0, 0, 128));
-			BufferedImage bgImg = slice.render(width, 48); // because we don't include the outer edges
-			bgImg.getGraphics().drawImage(textImg2, 3+(bgImg.getWidth()-textImg.getWidth())/2,
-					3+(bgImg.getHeight()-textImg.getHeight())/2, null);
-			bgImg.getGraphics().drawImage(textImg, (bgImg.getWidth()-textImg.getWidth())/2,
-					(bgImg.getHeight()-textImg.getHeight())/2, null);
+			
+			BufferedImage bgImg = slice.render(width, height); // because we don't include the outer edges
+			
+			if (shadow) {
+				BufferedImage shadowImg = ImageColorizer.colorizeImage(image, new Color(0, 0, 0, 128));
+				bgImg.getGraphics().drawImage(shadowImg, 3+(bgImg.getWidth()-image.getWidth())/2,
+						3+(bgImg.getHeight()-image.getHeight())/2, null);
+			}
+			bgImg.getGraphics().drawImage(image, (bgImg.getWidth()-image.getWidth())/2,
+					(bgImg.getHeight()-image.getHeight())/2, null);
 			imgs[i] = bgImg;
 		}
 		
-		return new Button(parent, id, x, y, width, 48, imgs);
+		return new Button(parent, id, x, y, width, height, imgs);
 	}
 
 }
