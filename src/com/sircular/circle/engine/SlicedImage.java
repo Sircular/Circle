31fdd64481957c106d19e648513c82830585f40e
Sircular <Sircular@github.com>
2014-08-04 20:17:22 -0600

    Added some names to SlicedImage.

--- src/com/sircular/circle/engine/SlicedImage.java
+++ src/com/sircular/circle/engine/SlicedImage.java
@@ -8,21 +8,31 @@ public class SlicedImage {
 	
 	private BufferedImage[] slices;
 	
+	private final int TOP_LEFT = 0;
+	private final int TOP_CENTER = 1;
+	private final int TOP_RIGHT = 2;
+	private final int CENTER_LEFT = 3;
+	private final int CENTER = 4;
+	private final int CENTER_RIGHT = 5;
+	private final int BOTTOM_LEFT = 6;
+	private final int BOTTOM_CENTER = 7;
+	private final int BOTTOM_RIGHT = 8;
+	
 	public SlicedImage(BufferedImage img, int tlx, int tly, int brx, int bry) {
 		slices = new BufferedImage[9];
 		
 		// top row
-		slices[0] = img.getSubimage(0, 0, tlx, tly);
-		slices[1] = img.getSubimage(tlx, 0, brx-tlx, tly);
-		slices[2] = img.getSubimage(brx, 0, img.getWidth()-brx, tly);
+		slices[TOP_LEFT] = img.getSubimage(0, 0, tlx, tly);
+		slices[TOP_CENTER] = img.getSubimage(tlx, 0, brx-tlx, tly);
+		slices[TOP_RIGHT] = img.getSubimage(brx, 0, img.getWidth()-brx, tly);
 		// middle row
-		slices[3] = img.getSubimage(0, tly, tlx, bry-tly);
-		slices[4] = img.getSubimage(tlx, tly, brx-tlx, bry-tly);
-		slices[5] = img.getSubimage(brx, tly, img.getWidth()-brx, bry-tly);
+		slices[CENTER_LEFT] = img.getSubimage(0, tly, tlx, bry-tly);
+		slices[CENTER] = img.getSubimage(tlx, tly, brx-tlx, bry-tly);
+		slices[CENTER_RIGHT] = img.getSubimage(brx, tly, img.getWidth()-brx, bry-tly);
 		// bottom row
-		slices[6] = img.getSubimage(0, bry, tlx, img.getHeight()-bry);
-		slices[7] = img.getSubimage(tlx, bry, brx-tlx, img.getHeight()-bry);
-		slices[8] = img.getSubimage(brx, bry, img.getWidth()-brx, img.getHeight()-bry);
+		slices[BOTTOM_LEFT] = img.getSubimage(0, bry, tlx, img.getHeight()-bry);
+		slices[BOTTOM_CENTER] = img.getSubimage(tlx, bry, brx-tlx, img.getHeight()-bry);
+		slices[BOTTOM_RIGHT] = img.getSubimage(brx, bry, img.getWidth()-brx, img.getHeight()-bry);
 	}
 	
 	public BufferedImage render(int width, int height) {
@@ -34,19 +44,19 @@ public class SlicedImage {
 		
 		Graphics g = renderedImage.getGraphics();
 		// draw the corners
-		g.drawImage(slices[0], 0, 0, null);
-		g.drawImage(slices[2], (int)bounds.getMaxX(), 0, null);
-		g.drawImage(slices[6], 0, (int) bounds.getMaxY(), null);
-		g.drawImage(slices[8], (int)bounds.getMaxX(), (int)bounds.getMaxY(), null);
+		g.drawImage(slices[TOP_LEFT], 0, 0, null);
+		g.drawImage(slices[TOP_RIGHT], (int)bounds.getMaxX(), 0, null);
+		g.drawImage(slices[BOTTOM_LEFT], 0, (int) bounds.getMaxY(), null);
+		g.drawImage(slices[BOTTOM_RIGHT], (int)bounds.getMaxX(), (int)bounds.getMaxY(), null);
 		
 		// draw the edges
-		g.drawImage(renderTiledSection(slices[1], (int)bounds.width, (int)bounds.getMinY()), (int)bounds.getMinX(), 0, null);
-		g.drawImage(renderTiledSection(slices[7], (int)bounds.width, height-(int)bounds.getMaxY()), (int)bounds.getMinX(), (int)bounds.getMaxY(), null);
-		g.drawImage(renderTiledSection(slices[3], (int)bounds.getMinX(), bounds.height), 0, (int)bounds.getMinY(), null);
-		g.drawImage(renderTiledSection(slices[5], width-(int)bounds.getMaxX(), bounds.height), (int)bounds.getMaxX(), (int)bounds.getMinX(), null);
+		g.drawImage(renderTiledSection(slices[TOP_CENTER], (int)bounds.width, (int)bounds.getMinY()), (int)bounds.getMinX(), 0, null);
+		g.drawImage(renderTiledSection(slices[BOTTOM_CENTER], (int)bounds.width, height-(int)bounds.getMaxY()), (int)bounds.getMinX(), (int)bounds.getMaxY(), null);
+		g.drawImage(renderTiledSection(slices[CENTER_LEFT], (int)bounds.getMinX(), bounds.height), 0, (int)bounds.getMinY(), null);
+		g.drawImage(renderTiledSection(slices[CENTER_RIGHT], width-(int)bounds.getMaxX(), bounds.height), (int)bounds.getMaxX(), (int)bounds.getMinX(), null);
 		
 		// draw the center
-		g.drawImage(renderTiledSection(slices[4], bounds.width, bounds.height), (int)bounds.getMinX(), (int)bounds.getMinY(), null);
+		g.drawImage(renderTiledSection(slices[CENTER], bounds.width, bounds.height), (int)bounds.getMinX(), (int)bounds.getMinY(), null);
 		
 		return renderedImage;
 	}
